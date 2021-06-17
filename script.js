bf89a237191b20dc8664793419db8c420ecf6931
Georgia <ghobs987@gmail.com>
2021-06-17 17:45:02 +0100

    indexs and javascript elements added

--- script.js
+++ script.js
@@ -5,9 +5,50 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+  
+}
 
+
+// lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+function generatePassword() {
+  var lengthGen = lengthGenerator();
+  var lowerC = lowerCaseGen();
+  var upperC = upperCaseGen();
+  var nums = numberGen();
+  var specialChars = specialCharGen();
+
+  // length between 8-128
+  function lengthGenerator() {
+    window.prompt("How Many characters do you want your password to have, between 8 and 128")
+  
+  }
+
+  function lowerCaseGen() {
+    var LCIndex = "abcdefghijklmnopqrstuvwxyz";
+    window.prompt("How Many characters do you want your password to have, between 8 and 128")
+  
+  }
+
+  function upperCaseGen() {
+    var UCIndex =   "ABCDEFGHIJKLMNOPQRSTUVWZYX";
+    window.prompt("How Many characters do you want your password to have, between 8 and 128")
+  
+  }
+
+  function numberGen() {
+    var NumIndex = "0123456789";
+    window.prompt("How Many characters do you want your password to have, between 8 and 128")
+  
+  }
+
+  function specialCharGen() {
+    var Specindex = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    window.prompt("How Many characters do you want your password to have, between 8 and 128")
+  
+  }
 }
 
 // Add event listener to generate button
