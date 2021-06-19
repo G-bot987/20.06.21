7413baef621623109f9ef3c148e51548199168b2
Georgia <ghobs987@gmail.com>
2021-06-19 19:38:06 +0100

    added if else logical operators for password characteristics

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
-  
+
 }
 
 
@@ -23,44 +23,103 @@ function generatePassword() {
   // length between 8-128
   function lengthGenerator() {
     var pWordLength = prompt("How Many characters do you want your password to have, between 8 and 128");
-    console.log(pWordLength)
+    if (pWordLength <= 128 && pWordLength >= 8) {
+      window.alert("you choose " + pWordLength);
+      console.log(pWordLength)
+    }
+    else { window.alert("you must choose a number between 8 and 128")
+      lengthGenerator() };
+    // recursion 
+    // pWordLength =prompt
+
   }
 
   function lowerCaseGen() {
     var LCIndex = "abcdefghijklmnopqrstuvwxyz".split(
       ''
-      );
-      
-      var lowerCAnswer = prompt("Do you want lower case letters")
-      console.log(lowerCAnswer)
+    );
+
+    var lowerCAnswer = prompt("Do you want lower case letters, Y or N");
+
+    lowerCAnswer = lowerCAnswer.toUpperCase();
+    if (lowerCAnswer == "Y") {
+      console.log("lower case yip");
+      window.alert("you choose " + lowerCAnswer);
+    } else if (lowerCAnswer == "N") {
+      window.alert("you choose " + lowerCAnswer)
+      console.log("lower case Nip");
+    } else {
+      window.alert("you must choose y or n, I believe in you");
+      lowerCaseGen()
+    };
+
+
   }
 
   function upperCaseGen() {
     var UCIndex = "ABCDEFGHIJKLMNOPQRSTUVWZYX".split(
       ''
     );
-    var upperCAnswer = prompt("Do you want upper case letters")
-      console.log(upperCAnswer)
-  
+
+    var upperCAnswer = prompt("Do you want upper case letters, Y or N")
+
+    upperCAnswer = upperCAnswer.toUpperCase();
+    if (upperCAnswer == "Y") {
+      console.log("upper case yip");
+      window.alert("you choose " + upperCAnswer);
+    } else if (upperCAnswer == "N") {
+      window.alert("you choose " + upperCAnswer)
+      console.log("upper case Nip");
+    } else {
+      window.alert("you must choose y or n, I believe in you");
+      upperCaseGen()
+    };
+
+
   }
 
   function numberGen() {
     var NumIndex = "0123456789".split(
       ''
     );
-    
-    var numAnswer = prompt("Do you want numbers")
-      console.log(numAnswer)
-  
+
+    var numAnswer = prompt("Do you want numbers Y or N")
+
+    numAnswer = numAnswer.toUpperCase();
+    if (numAnswer == "Y") {
+      console.log("numbers yip");
+      window.alert("you choose " + numAnswer);
+    } else if (numAnswer == "N") {
+      window.alert("you choose " + numAnswer)
+      console.log("numbers Nip");
+    } else {
+      window.alert("you must choose y or n, I believe in you");
+      numberGen()
+    };
+
+
   }
 
   function specialCharGen() {
-    var Specindex = "!#$%&'()*+,-.\"/:;<=>?@[]^_`\\{|}~".split( 
+    var Specindex = "!#$%&'()*+,-.\"/:;<=>?@[]^_`\\{|}~".split(
       ''
     );
-    var specialAnswer = prompt("Do you want special characters")
-      console.log(specialAnswer)
-  
+    var specialAnswer = prompt("Do you want special characters, Y or N")
+
+    specialAnswer = specialAnswer.toUpperCase();
+    if (specialAnswer == "Y") {
+      console.log("special yip");
+      window.alert("you choose " + specialAnswer);
+    } else if (specialAnswer == "N") {
+      window.alert("you choose " + specialAnswer)
+      console.log("numbers Nip");
+    } else {
+      window.alert("you must choose y or n, I believe in you");
+      specialCharGen()
+    };
+    
+    
+
   }
 }
 
